8b01bf790b736d2e1cf88c21bc458bd34b102c30
janvi kachchhi <janukachchhi2@gmail.com>
2023-04-13 12:55:42 +0530

    Initial Commit

---
+++ GIVAApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GIVAApplication
+//
+//  Created by R93 on 13/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
